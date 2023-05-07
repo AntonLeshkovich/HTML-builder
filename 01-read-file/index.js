987ed08f71be77a536b510de51ada0b631aaa023
Anton Leshkovich <antonleshkovich@mail.ru>
2023-05-07 19:59:13 +0300

    feat: update js file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,5 +3,5 @@ const fs = require('fs');
 const readStream = fs.createReadStream('./01-read-file/text.txt');
 
 readStream.on('data', (chunk) => {
-    console.log(chunk.toString());
+  console.log(chunk.toString());
 });
